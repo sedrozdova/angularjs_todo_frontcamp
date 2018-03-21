e63fa15f014510168dc689d444f4eb1c5e5449e0
Karalkou <yury.karalkou@gmail.com>
2018-03-21 08:46:39 +0300

    Add sorting by sortType (see three buttons). Activate todo task edit.

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 0. ~~Create webpack config and common project structure~~
-1. Add AngularJS 1.#.# to a project
-2. Add minimum functionality
-    1. Add status(is it done or not) and date creation to todo model
-    2.Create a view displaying two lists of todos: “Done todos” and “New todos”
-    3. Add filtering for todo list on base how old is the todo in days
-3. Add more functionality
-    1. Add validation to the form
-        1. Fields "Text" should be mandatory
-        2. Create a custom validator that check the minimum length of a todo text: 20 symbols.
-    2. Reuse the form above to edit an article by clicking on article title
-    3. Add filtering by first letter and by date
+1. ~~Add AngularJS 1.#.# to a project~~
+2. ~~Add minimum functionality~~
+    1. ~~Add status(is it done or not) and date creation to todo model~~
+    2. ~~Create a view displaying two lists of todos: “Done todos” and “New todos”~~
+    3. ~~Add filtering for todo list on base how old is the todo in days~~
+3. ~~Add more functionality~~
+    1. ~~Add validation to the form~~
+        1. ~~Fields "Text" should be mandatory~~
+        2. ~~Create a custom validator that check the minimum length of a todo text: 20 symbols.~~
+    2. ~~Reuse the form above to edit an article by clicking on article title~~
+    3. ~~Add sorting by first letter and by date~~
 4. Add routes and resource
     1. "Add todo"/"Edit todo" forms should be opened inside different views.
     2. Make sure that "Add todo"/"Edit todo" views have different routes, i.e.
--- src/todo.js
+++ src/todo.js
@@ -7,25 +7,22 @@ import './index.scss';
 
 const app = angular.module('todoApp', ['ngRoute']);
 
-app.filter('customDate', function() {
-    return function(d) {
+app.filter('customDate', function () {
+    return function (d) {
         return moment(d).format('DD/MM/YYYY hh:mm');
     };
 });
 
-app.directive('min20symbols', function() {
+app.directive('min20symbols', function () {
     return {
         require: 'ngModel',
-        link: function(scope, elm, attrs, ctrl) {
-            ctrl.$validators.lengthCheck = function(modelValue, viewValue) {
-                if (ctrl.$isEmpty(modelValue)) {
+        link: function (scope, element, attr, ngModel) {
+            ngModel.$validators.lengthCheck = function (modelValue, viewValue) {
+                if (ngModel.$isEmpty(modelValue)) {
                     return true;
                 }
 
-                if (viewValue.length < 20) {
-                    return false;
-                }
-                return true;
+                return viewValue.length > 20;
             };
         }
     };
@@ -35,19 +32,25 @@ app.factory('todoFactory', () => {
     const tasksList = [
         {
             id: 0,
-            text: 'Yesterdays\' task',
+            text: 'Yesterdays task',
             done: false,
             createdAt: moment().subtract(1, 'days')
         },
         {
             id: 1,
+            text: 'Couple of days ago task',
+            done: false,
+            createdAt: moment().subtract(2, 'days')
+        },
+        {
+            id: 2,
             text: 'A week ago',
             done: true,
             createdAt: moment().subtract(7, 'days')
         },
         {
-            id: 2,
-            text: 'Today\'s task',
+            id: 3,
+            text: 'Today task',
             done: false,
             createdAt: moment()
         },
@@ -71,22 +74,42 @@ app.factory('todoFactory', () => {
         addTodo(todo) {
             tasksList.push(todo);
         },
+        changeTodoTitle: function(todoId, newTitle) {
+            let index = tasksList.findIndex(elem => elem.id === todoId);
+            tasksList[index].text = newTitle;
+        },
         completeTask(task) {
             let index = tasksList.findIndex((el) => el === task);
             tasksList[index].done = !tasksList[index].done;
+        },
+    };
+});
+
+app.filter('startsWithLetter', function () {
+    return function (items, letter) {
+        let filtered = [];
+        let letterMatch = new RegExp(letter, 'i');
+        for (let i = 0; i < items.length; i++) {
+            let item = items[i];
+            if (letterMatch.test(item.text.substring(0, 1))) {
+                filtered.push(item);
+            }
         }
+        return filtered;
     };
 });
 
 app.controller('todoController', ['$scope', 'todoFactory', ($scope, todoFactory) => {
     $scope.completedTasks = todoFactory.getCompletedTasks();
+    $scope.tasks = todoFactory.getTasks();
     $scope.newTasks = todoFactory.getNewTasks();
     $scope.newTodo = '';
     $scope.filter = -1;
     $scope.textFilter = '';
+    $scope.sortType = '';
+    $scope.changingTodoId = '';
 
     $scope.addTodo = () => {
-        console.log($scope.newTodo);
         let todo = {
             text: $scope.newTodo,
             id: uuid.v1(),
@@ -94,7 +117,7 @@ app.controller('todoController', ['$scope', 'todoFactory', ($scope, todoFactory)
             createdAt: moment()
         };
 
-        if(!todo.text) {
+        if (!todo.text) {
             alert('Text of todo is mandatory...');
             return;
         }
@@ -104,25 +127,37 @@ app.controller('todoController', ['$scope', 'todoFactory', ($scope, todoFactory)
         $scope.completedTasks = todoFactory.getCompletedTasks();
         $scope.newTasks = todoFactory.getNewTasks();
     };
-
+    $scope.editTodo = function() {
+        let newTitle = $scope.newTodo;
+        todoFactory.changeTodoTitle($scope.changingTodoId, newTitle);
+        $scope.newTodo = '';
+        $scope.changingTodoId = '';
+    };
     $scope.completeTask = (task) => {
         todoFactory.completeTask(task);
         $scope.completedTasks = todoFactory.getCompletedTasks();
         $scope.newTasks = todoFactory.getNewTasks();
     };
-
-    $scope.dateFilter = task => {
-        if($scope.filter === -1) return true;
-        return Math.abs(moment(task.createdAt).diff(moment(), 'days')) <= $scope.filter;
+    $scope.activateEditTodo = function(id) {
+        console.log('$scope: ', $scope);
+        $scope.changingTodoId = id;
+        $scope.newTodo = $scope.tasks.find(elem => elem.id === id).text;
     };
 
     $scope.dateFilter = task => {
-        if($scope.textFilter.length === 0) return true;
-        return task.text.toLowerCase().indexOf($scope.textFilter.toLowerCase()) > -1;
+        if ($scope.filter === -1) {
+            return true;
+        }
+
+        return Math.abs(moment(task.createdAt).diff(moment(), 'days')) <= $scope.filter;
     };
 
     $scope.changeFilter = filter => {
         $scope.filter = filter;
+    };
+
+    $scope.changeTodoSortType = function(type) {
+        $scope.sortType = type;
     };
 
 }]);
